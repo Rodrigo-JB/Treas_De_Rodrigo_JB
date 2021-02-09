7f606ba79d70b266fb46662a0a8d94d8a1fa8ebd
Rodrigo-JB <78760701+Rodrigo-JB@users.noreply.github.com>
2021-02-09 01:26:33 -0600

    Add files via upload

---
+++ Tarea-1.py
@@ -0,0 +1,9 @@
+print("¿Cual es tu nombre?")
+
+Nombres = input()
+
+P1_apellido = input("¿Cual es tu primer apellido? ")
+
+S2_apellido = input("¿Cual es tu segundo apellido? ")
+
+print("Mi nombre es", Nombres, P1_apellido, S2_apellido)
